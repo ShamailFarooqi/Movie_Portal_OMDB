fa7249b77f03ac1912da22de99f3e1d661dd1968
ShamailFarooqi <shumailfarooqi12@gmail.com>
2024-01-02 23:33:44 +0530

    updated video src

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./page.module.css"
 import Link from "next/link";
+import videoData from "../../public/video/video.mp4"
 export default function Home() {
   return  (
       <main className={styles.main}>
@@ -11,7 +12,7 @@ export default function Home() {
               </div>
           </div>
           <video
-              src={require("../../public/video/video.mp4")}
+              src="https://ia600300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
               autoPlay
               muted
               loop
